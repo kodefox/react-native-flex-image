fa049dcbee7e89ea93b25c5bf14d8b0b4ba88f2c
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2018-03-15 16:10:09 +0700

    Implement progressive image

--- examples/src/lib/__tests__/FlexImage-test.js
+++ examples/src/lib/__tests__/FlexImage-test.js
@@ -1,14 +1,78 @@
-import {getImageSize} from '../FlexImage';
+import React from 'react';
+import FlexImage, {getImageSize} from '../FlexImage';
+import renderer from 'react-test-renderer';
 
-jest.mock('react-native', () => ({
-  Image: {
-    getSize: jest.fn((uri, callback) => {
-      setTimeout(() => {
-        callback(200, 300);
-      }, 1);
-    }),
-  },
+jest.mock('Image', () => ({
+  getSize: jest.fn((uri, callback) => {
+    setTimeout(() => {
+      callback(200, 300);
+    }, 1);
+  }),
 }));
+
+jest.mock('Animated', () => ({
+  Image: 'Animated.Image',
+  View: 'Animated.View',
+  createTimer: jest.fn(),
+  Value: class AnimatedValue {
+    constructor(value) {
+      return {
+        interpolate: () => value,
+        __getValue: () => value,
+      };
+    }
+  },
+  timing: jest.fn(() => ({
+    start: (callback) => callback && callback(),
+  })),
+}));
+
+function findElement(json, testID) {
+  if (json) {
+    if (json.props && json.props.testID === testID) {
+      return json;
+    } else if (json.children && json.children.length > 0) {
+      for (let x of json.children) {
+        let result = findElement(x, testID);
+        if (result) {
+          return result;
+        }
+      }
+    }
+  }
+}
+
+it('should render thumbnail when loadingMethod is progressive', () => {
+  let component = renderer.create(
+    <FlexImage
+      source={{
+        uri: 'https://i.imgur.com/v0Y4Jfu.jpg',
+      }}
+      thumbnail={{uri: 'https://i.imgur.com/ngVArkT.jpg'}}
+      loadingMethod="progressive"
+    />
+  );
+  component.getInstance()._onLoadSuccess();
+  let element = findElement(component.toJSON(), 'progressiveThumbnail');
+  const anonymous = () => jest.fn();
+  let props = {
+    source: {uri: 'https://i.imgur.com/ngVArkT.jpg'},
+    style: {
+      width: '100%',
+      height: '100%',
+      opacity: {
+        interpolate: () => jest.fn(),
+        __getValue: () => jest.fn(),
+      },
+    },
+    onLoad: anonymous,
+    testID: 'progressiveThumbnail',
+  };
+  expect(Object.keys(element)).toEqual(['type', 'props', 'children']);
+  expect(element.type).toBe('Animated.Image');
+  expect(JSON.stringify(element.props)).toEqual(JSON.stringify(props));
+  expect(element.children).toBeNull();
+});
 
 it('should run onSuccess function when width and height are resolved', async() => {
   let onSuccess;
