baf02a62ad476870c81f8041fcd9fa52ba1d5b75
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-01-06 04:36:52 +0700

    Compile main file

---
+++ lib/FlexImage.d.ts
@@ -0,0 +1,47 @@
+import { Component, ReactElement } from 'react';
+import { Animated, ViewStyle, StyleProp } from 'react-native';
+declare type Cancellable = {
+    cancel: () => void;
+};
+declare type Source = number | {
+    uri: string;
+    width?: number;
+    height?: number;
+};
+declare type Props = {
+    source: number | Source;
+    style?: StyleProp<ViewStyle>;
+    loadingComponent?: ReactElement<any>;
+    onPress?: () => void;
+    thumbnail?: number | Source;
+    loadingMethod?: 'spinner' | 'progressive';
+    errorComponent?: ReactElement<any>;
+};
+declare type State = {
+    isLoading: boolean;
+    ratio: number;
+    error: string;
+    thumbnailOpacity: Animated.Value;
+};
+export default class FlexImage extends Component<Props, State> {
+    _pendingGetSize: Cancellable | null;
+    state: {
+        isLoading: boolean;
+        ratio: number;
+        error: string;
+        thumbnailOpacity: Animated.Value;
+    };
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    render(): JSX.Element;
+    _onThumbnailLoad: () => void;
+    _onLoad: () => void;
+    _onLoadSuccess: (width: number, height: number) => void;
+    _onLoadFail: (error: Error) => void;
+}
+export declare function getImageSize(source: {
+    uri: string;
+}, onSuccess: (width: number, height: number) => void, onFail: (error: Error) => void): {
+    cancel: () => void;
+};
+export {};
