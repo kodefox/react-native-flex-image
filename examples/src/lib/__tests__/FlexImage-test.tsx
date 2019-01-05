3d7d65658b5c0df1a5884024d883a164e37b8dcf
Yoan Pratama Putra <yoanpratamaputra@gmail.com>
2019-01-06 03:50:09 +0700

    Make unit test works again

--- examples/src/lib/__tests__/FlexImage-test.tsx
+++ examples/src/lib/__tests__/FlexImage-test.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
 import FlexImage, {getImageSize} from '../FlexImage';
-// import {getImageSize} from '../FlexImage';
-import renderer from 'react-test-renderer';
+import {render} from 'react-native-testing-library';
 
 type Callback = (width: number, height: number) => void;
-// type Json = {
-//   props: {testID: string};
-//   children: Array<Json>;
-// };
 
 jest.mock('Image', () => ({
   getSize: jest.fn((_uri: string, callback: Callback) => {
@@ -34,53 +29,17 @@ jest.mock('Animated', () => ({
   })),
 }));
 
-// function findElement(json: Json, testID: string): Json | void {
-//   if (json) {
-//     if (json.props && json.props.testID === testID) {
-//       return json;
-//     } else if (json.children && json.children.length > 0) {
-//       for (let x of json.children) {
-//         let result = findElement(x, testID);
-//         if (result) {
-//           return result;
-//         }
-//       }
-//     }
-//   }
-// }
-
 it('should render thumbnail when loadingMethod is progressive', () => {
-  let component = renderer.create(
+  let component = render(
     <FlexImage
       source={{
         uri: 'https://i.imgur.com/v0Y4Jfu.jpg',
       }}
       thumbnail={{uri: 'https://i.imgur.com/ngVArkT.jpg'}}
       loadingMethod="progressive"
-    />
+    />,
   );
-  // component.getInstance()._onLoadSuccess();
-  // let element = findElement(component.toJSON(), 'progressiveThumbnail');
-  // const anonymous = () => jest.fn();
-  // let props = {
-  //   source: {uri: 'https://i.imgur.com/ngVArkT.jpg'},
-  //   style: {
-  //     width: '100%',
-  //     height: '100%',
-  //     opacity: {
-  //       interpolate: () => jest.fn(),
-  //       __getValue: () => jest.fn(),
-  //     },
-  //     zIndex: 1,
-  //   },
-  //   onLoad: anonymous,
-  //   testID: 'progressiveThumbnail',
-  // };
-  // expect(Object.keys(element)).toEqual(['type', 'props', 'children']);
-  // expect(element.type).toBe('Animated.Image');
-  // expect(JSON.stringify(element.props)).toEqual(JSON.stringify(props));
-  // expect(element.children).toBeNull();
-  expect(component).toBeTruthy();
+  expect(component.toJSON()).toMatchSnapshot();
 });
 
 it('should run onSuccess function when width and height are resolved', async () => {
